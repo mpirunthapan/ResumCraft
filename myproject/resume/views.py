68a372b486a93181f93065c62f8c4a081e4a4246
mpirunthapan <mpirunthapan@gmail.com>
2025-12-03 22:21:41 +0530

    Third Commit Finished Resume app with windows.print

--- myproject/resume/views.py
+++ myproject/resume/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .forms import ProfileForm
 from .models import Profile
 
@@ -11,12 +11,12 @@ def accept(request):
         form = ProfileForm(request.POST)
         if form.is_valid():
             form.save()
-            return render(request, "index.html")  # create this page
+            return render(request, "index.html")  
     else:
         form = ProfileForm()
 
     return render(request, "accept.html", {"form": form})
 
 def preview(request, profile_id):
-    profile = Profile.objects.get(id=profile_id)
+    profile = get_object_or_404(Profile, id=profile_id)
     return render(request, "preview.html", {"profile": profile})
